3ddebfb37b013d2cfb4b281fc783b8bf8408cae1
Asif Mallik <mdasifmallik2@gmail.com>
2020-12-17 22:44:15 +0600

    Final App

--- src/router/index.js
+++ src/router/index.js
@@ -9,27 +9,42 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: "Home | Products"
+    }
   },
   {
     path: '/add_product',
     name: 'AddProduct',
-    component: () => import('../views/AddProduct.vue')
+    component: () => import('../views/AddProduct.vue'),
+    meta: {
+      title: "Add Product"
+    }
   },
   {
     path: '/edit_product/:id',
     name: 'EditProduct',
-    component: () => import('../views/EditProduct.vue')
+    component: () => import('../views/EditProduct.vue'),
+    meta: {
+      title: "Edit Product"
+    }
   },
   {
     path: '/login',
     name: 'Login',
-    component: () => import('../views/auth/Login.vue')
+    component: () => import('../views/auth/Login.vue'),
+    meta: {
+      title: "Login | Products"
+    }
   },
   {
     path: '/register',
     name: 'Register',
-    component: () => import('../views/auth/Register.vue')
+    component: () => import('../views/auth/Register.vue'),
+    meta: {
+      title: "Register | Products"
+    }
   },
   {
     path: '/*',
@@ -45,6 +60,7 @@ const router = new VueRouter({
 
 // Navigation Guard
 router.beforeEach((to, from, next) => {
+  window.document.title = to.meta && to.meta.title ? to.meta.title : 'Products';
   if (to.name === "Login" || to.name === "Register") {
     // Redirect to Home if there is valid token
     if (user.hasValidToken()) {
